41f574e34185ff395a42161850a7b6bdac33e350
LIL-Giraffe <hdbest33@gmail.com>
2023-11-04 16:49:34 +0530

    watchpage

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ import Body from './components/Body';
 import Head from './components/Head';
 import { Provider } from "react-redux";
 import store from './utils/store';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import MainContainer from './components/MainContainer';
+import WatchPage from './components/WatchPage';
 
 function App() {
+  const appRouter=createBrowserRouter([{
+    path:'/',
+    element:<Body/>,
+    children:[
+      {
+        path:'/',
+        element:<MainContainer/>
+      },
+      {
+        path:'watch',
+        element:<WatchPage/>
+      }
+    ]
+  }])
   return (
     <Provider store={store}>
     <div>
       <Head/>
-      <Body/>
+      <RouterProvider router={appRouter}/>
       {/* 
         - Head
         - Sidebar
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { YOUTUBE_API } from '../utils/constants'
 import VideoCard from './VideoCard'
+import { Link } from 'react-router-dom'
 
 const VideoContainer = () => {
-  const [videos,setVideos]=useState([])
-  const getVideo=async()=>{
-    const res=await fetch(YOUTUBE_API)
-    const json=await res.json()
+  const [videos, setVideos] = useState([])
+  const getVideo = async () => {
+    const res = await fetch(YOUTUBE_API)
+    const json = await res.json()
     setVideos(json.items)
   }
-  useEffect(()=>{
+  useEffect(() => {
     getVideo()
-  },[])
+  }, [])
 
   return (
     <div className='flex flex-wrap'>
       {
-        videos.map((video)=>(<VideoCard info={video}/>))
+        videos.map((video) => (
+          <Link to={"/watch?v=" + video.id}><VideoCard info={video} /></Link>
+        ))
       }
     </div>
   )
